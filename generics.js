fab46bab826e7d76f5421b3429506b3a91fc2b4f
0x21106 <ayhan2015007@gmail.com>
2020-03-17 17:05:26 +0400

    new things

---
+++ generics.js
@@ -0,0 +1,10 @@
+function value(x) {
+    return x;
+}
+var sayi = value(2);
+function value2(x) {
+    return x;
+}
+var sayi2 = value2(20);
+var sehir = value2("Baku");
+console.log(sehir);
--- generics.ts
+++ generics.ts
@@ -2,3 +2,12 @@ function value(x: number): number {
     return x;
 }
 
+let sayi = value(2);
+
+function value2<T>(x: T): T {
+    return x;
+}
+
+let sayi2 = value2<number>(20);
+let sehir = value2<string>("Baku");
+console.log(sehir);
